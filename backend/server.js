f43cd6169095a66918cd1f75adbb6ab3e2925485
Gabriel Guimarães <gabrielggartist@gmail.com>
2026-05-13 17:29:05 -0300

    fix: cors preflight netlify

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ['https://nexus-rpg-system.netlify.app/'],
-    credentials: true
+    origin: ['https://nexus-rpg-system.netlify.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
